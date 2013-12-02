5a4d2b359522f5cc96f0c419cd71b6a6df189397
Lucian Pacurar <luci.pacurar@gmail.com>
2013-12-02 10:16:01 +0200

    Added configurable header token name to request buffer.

--- services/bmAuthRequestBuffer.js
+++ services/bmAuthRequestBuffer.js
@@ -1,35 +1,44 @@
-angular.module("bmComponents").factory("bmAuthRequestBuffer", ["$rootScope", "$injector",
-    function ($rootScope, $injector) {
+angular.module("bmComponents").provider("bmAuthRequestBuffer",
+    function () {
 
-        var buffer = [];
+        var that = this;
 
-        function retry(config, deferred) {
-            var $http = $injector.get("$http");
-            // remove old headers (Angular 1.1.x only);
-            delete config.headers["Auth-Token"];
-            $http(config).then(function(response) {
-                deferred.resolve(response);
-            });
-        }
+        this.tokenHeaderName = "Auth-Token";
 
-        function retryAll() {
-            for (var i = 0; i < buffer.length; ++i) {
-                retry(buffer[i].config, buffer[i].deferred);
+        this.$get = ["$rootScope", "$injector",
+            function ($rootScope, $injector) {
+
+                var buffer = [];
+
+                function retry(config, deferred) {
+                    var $http = $injector.get("$http");
+                    // remove old headers (Angular 1.1.x only);
+                    delete config.headers[that.tokenHeaderName];
+                    $http(config).then(function(response) {
+                        deferred.resolve(response);
+                    });
+                }
+
+                function retryAll() {
+                    for (var i = 0; i < buffer.length; ++i) {
+                        retry(buffer[i].config, buffer[i].deferred);
+                    }
+                    buffer = [];
+                }
+
+                return {
+                    loginConfirmed: function() {
+                        $rootScope.$broadcast("bmLoginConfirmed");
+                        retryAll();
+                    },
+                    pushToBuffer: function(config, deferred) {
+                        buffer.push({
+                            config: config,
+                            deferred: deferred
+                        })
+                    }
+                }
             }
-            buffer = [];
-        }
-
-        return {
-            loginConfirmed: function() {
-                $rootScope.$broadcast("bmLoginConfirmed");
-                retryAll();
-            },
-            pushToBuffer: function(config, deferred) {
-                buffer.push({
-                    config: config,
-                    deferred: deferred
-                })
-            }
-        }
+        ];
     }
-]);
+);
