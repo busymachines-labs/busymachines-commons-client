ba0f291f98ae240fbf7238091c64383f30c3a9d4
Lucian Pacurar <luci.pacurar@gmail.com>
2014-03-19 17:18:28 +0200

    Add bmPhoneNumber directive.

---
+++ directives/bmPhoneNumber.js
@@ -0,0 +1,29 @@
+angular.module("bmComponents").directive("bmPhoneNumber", [
+    function () {
+
+        return {
+            restrict: "A",
+            require: "ngModel",
+            link: function(scope, element, attrs, ngModel) {
+
+                ngModel.$parsers.unshift(function(viewValue) {
+                    var phoneRegExp = /^(\s*[\d+-?\+?]\s*)*$/g,
+                        regexpValid = phoneRegExp.test(viewValue);
+//                        minlengthValid = (viewValue.length >= 5),
+//                        maxlengthValid = (viewValue.length <= 20),
+//                        isValid = regexpValid && minlengthValid && maxlengthValid;
+
+                    if (regexpValid) {
+                        // it is valid
+                        ngModel.$setValidity("phone", true);
+                        return viewValue;
+                    } else {
+                        // it is invalid, return undefined (no model update)
+                        ngModel.$setValidity("phone", false);
+                        return undefined;
+                    }
+                });
+            }
+        }
+    }
+]);
