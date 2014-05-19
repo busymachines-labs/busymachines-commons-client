90681f1a726580d29b3b7ac76188c378b96b53bc
cristi <cristif3@gmail.com>
2014-05-19 18:10:59 +0300

    added map-key and zoom attributes for bmMapBox

--- bmComponents.js
+++ bmComponents.js
@@ -189,9 +189,16 @@ angular.module("bmComponents", []);;angular.module("bmComponents").directive("bm
         return {
             restrict : "A",
             link : function(scope, element, attrs) {
-                var map = L.mapbox.map(element.get(0), "agilexs.ggma3d04").setView([52.285, 5.724], 7),
+                var map = L.mapbox.map(element.get(0), attrs.mapKey).setView([52.285, 5.724], 7),
                     markerLayer = L.mapbox.markerLayer().addTo(map),
-                    mapLoaded = false;
+                    mapLoaded = false,
+                    zoom,
+                    mapKey;
+
+                if (attrs.zoom) {
+                    zoom = scope.$eval(attrs.zoom);
+                    map.setView([52.285, 5.724], zoom);
+                }
 
                 scope.$watch(attrs.geoJson, function (val) {
                     if (val) {
